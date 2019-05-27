62deb573baedd59f2e6d4f335f3879e4701f5069
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-27 19:30:35 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,9 +4,9 @@ class Appointment
   attr_accessor :patient, :doctor, :date
   
   def initialize(patient, doctor, date)
-    @patient= patient
-    @doctor= doctor
-    @date= date
+    @patient = patient
+    @doctor = doctor
+    @date = date
     @@all << self
   end
   
