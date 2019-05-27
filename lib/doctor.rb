640d1f348092883a0bd96f5838f971583476b873
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-27 19:01:28 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,7 +22,7 @@ class Doctor
   end
   
   def patients
-    self.appointments.collect {|appointment| appointment.patient}
+    self.appointments.collect {|appointment| binding.pry}
   end
   
 end
